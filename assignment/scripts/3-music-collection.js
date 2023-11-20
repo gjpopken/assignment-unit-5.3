2b98009e2f8f590a52cd514d9c2011c6dffc0c26
Gavin Popken <gavinpopkenart@gmail.com>
2023-11-20 15:47:11 -0600

    setup var and function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,7 +1,10 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
+let myCollection = [];
 
+function addToCollection(collection, title, artist, yearPublished) {
 
+}
 
 
 
